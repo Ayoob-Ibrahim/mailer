a65eb97333731c70c48fcee160e4eaf6115c45e0
eitriyaz <riyaz@eitworks.com>
2025-04-20 07:21:11 -0700

    sdsd

--- api/index.ts
+++ api/index.ts
@@ -41,6 +41,18 @@ const generateHtmlEmail = (data) => {
 app.post("/send-email", async (req, res) => {
   const { name, phone, address, clients, email } = req.body;
   console.log(name, phone, address, clients, email);
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   const htmlContent = generateHtmlEmail({
     name,
     phone,
